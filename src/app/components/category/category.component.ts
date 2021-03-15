d08efaabd025c3087e99202567c3eca649f0d892
mustafa cagatay yılmaz <cagatay.yilmaz@ozan.com>
2021-03-15 22:17:39 +0300

    Updated category

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
+  categories: Category[] = [];
+  currentCategory: Category;
+  dataLoaded = false;
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
+    this.getCategories();
   }
 
+  getCategories() {
+    this.categoryService.getProducts().subscribe((response) => {
+      this.categories = response.data;
+      this.dataLoaded = true;
+    });
+  }
+
+  setCurrentCategory(category: Category) {
+    this.currentCategory = category;
+  }
+
+  getCategoryClass(category: Category){
+    if(category == this.currentCategory){
+      return 'list-group-item  active';
+    }
+    return 'list-group-item';
+  }
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,17 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Category } from '../models/category';
 import { ListResponseModel } from '../models/listResponseModel';
-import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
-  apiUrl = 'https://localhost:26622/api/Products/getall';
+export class CategoryService {
+  apiUrl = 'https://localhost:26622/api/Categories/getall';
   constructor(private httpClient: HttpClient) { }
 
-  getProducts():Observable<ListResponseModel<Product>> {
-    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
+  getProducts():Observable<ListResponseModel<Category>> {
+    return this.httpClient.get<ListResponseModel<Category>>(this.apiUrl);
   }
 }
