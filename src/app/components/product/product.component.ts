55fbaac982c019a607afc4c6c7215ea9dbcd2d40
mustafa cagatay yılmaz <cagatay.yilmaz@ozan.com>
2021-03-19 13:57:50 +0300

    Finalized cart operations

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -18,7 +19,8 @@ export class ProductComponent implements OnInit {
   constructor(
     private productService: ProductService,
     private activatedRoute: ActivatedRoute,
-    private toastrService: ToastrService
+    private toastrService: ToastrService,
+    private cartService: CartService
   ) {}
 
   ngOnInit(): void {
@@ -48,7 +50,8 @@ export class ProductComponent implements OnInit {
   }
 
   addToCart(product:Product) {
-    this.toastrService.success("Sepete eklendi");
-    console.log(product);
+    this.cartService.addToCart(product);
+    this.toastrService.success("Added to cart");
+    
   }
 }
